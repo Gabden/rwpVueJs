8aeda125351de0597dcf0df9518b93971daf0227
Denis <gabden5545@gmail.com>
2020-04-15 09:42:31 +0300

    add svg ti component

--- src/components/BaseIcon.vue
+++ src/components/BaseIcon.vue
@@ -1,11 +1,41 @@
 <template>
-  <div>
-    BaseIcon
+  <div class="icon-wrapper">
+    <svg class="icon" :width="width" :height="heigth">
+      <use v-bind="{ 'xlink:href': '/acid.svg#' + name }" />
+    </svg>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    name: String,
+    width: {
+      type: [String, Number],
+      default: 24
+    },
+    heigth: {
+      type: [String, Number],
+      default: 24
+    }
+  }
+}
 </script>
 
-<style></style>
+<style scoped>
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 1rem;
+  font-weight: 600;
+}
+.icon {
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+  margin-right: 6px;
+}
+</style>
